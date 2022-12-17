24e1c25d8e26886dfafab1a825afb0137db57a0b
Innnakentiya <innnakentiya@gmail.com>
2022-12-17 18:06:25 +0200

    Added API integration

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather />
+        <Weather defaultCity="London" />
         <footer>
           <a
             href="https://github.com/Innnakentiya/react-weather-app"
--- src/Weather.js
+++ src/Weather.js
@@ -1,82 +1,112 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./Weather.css";
 import ColWeatherForecast from "./ColWeatherForecast.js";
 import "./ColWeatherForecast.css";
 import image from "./images/sun.svg";
 
-export default function Weather() {
-  return (
-    <div className="weather">
-      <div className="Search">
-        <form id="search-form">
-          <input
-            type="text"
-            placeholder="Type a city"
-            id="city-input"
-            autoComplete="off"
-          />
-          <input type="submit" value="Search" />
-        </form>
-      </div>
-      <div className="location">
-        <h1>
-          5-Day Weather Forecast for <span className="city">Madrid</span>
-        </h1>
-      </div>
-      <div className="weather-forecast">
-        <div className="current-weather-forecast">
-          <div className="CurrentWeatherValue">
-            <div className="row">
-              <div className="col-2 text-center">
-                <img
-                  className="weather-icon-main"
-                  src={image}
-                  alt="Weather Icon"
-                  id="icon"
-                  width="90"
-                />
-                <div
-                  className="temperature-description"
-                  id="temperature-description"
-                >
-                  Sunny
+export default function Weather(props) {
+  const [ready, setReady] = useState(false);
+  const [weatherData, setWeatherData] = useState({});
+  function handleResponse(response) {
+    console.log(response.data);
+    setWeatherData({
+      city: response.data.name,
+      temperature: response.data.main.temp,
+      humidity: response.data.main.humidity,
+      wind: response.data.wind.speed,
+      description: response.data.weather[0].description,
+    });
+    setReady(true);
+  }
+
+  if (ready) {
+    return (
+      <div className="weather">
+        <div className="Search">
+          <form id="search-form">
+            <input
+              type="text"
+              placeholder="Type a city"
+              id="city-input"
+              autoComplete="off"
+            />
+            <input type="submit" value="Search" />
+          </form>
+        </div>
+        <div className="location">
+          <h1>
+            5-Day Weather Forecast for{" "}
+            <span className="city">{props.defaultCity}</span>
+          </h1>
+        </div>
+        <div className="weather-forecast">
+          <div className="current-weather-forecast">
+            <div className="CurrentWeatherValue">
+              <div className="row">
+                <div className="col-2 text-center">
+                  <img
+                    className="weather-icon-main"
+                    src={image}
+                    alt="Weather Icon"
+                    id="icon"
+                    width="90"
+                  />
+                  <div
+                    className="temperature-description text-capitalize"
+                    id="temperature-description"
+                  >
+                    {weatherData.description}
+                  </div>
                 </div>
-              </div>
-              <div className="col-6">
-                <div className="current-day">Today</div>
-                <div className="current-day-temperature" id="temperature-input">
-                  <span className="min-temp">+15℃</span>
-                  <span className="max-temp"> ...+15℃</span>
+                <div className="col-6">
+                  <div className="current-day">Today</div>
+                  <div
+                    className="current-day-temperature"
+                    id="temperature-input"
+                  >
+                    <span className="min-temp">+15℃</span>
+                    <span className="max-temp"> ...+15℃</span>
+                  </div>
                 </div>
-              </div>
-              <div className="col-4">
-                <ul>
-                  <li>
-                    Current temperature :<span id="current-temperature">5</span>
-                    ℃
-                  </li>
-                  <li>
-                    Humidity: <span id="humidity">67</span>%
-                  </li>
-                  <li>
-                    Wind: <span id="wind">3</span>km/h
-                  </li>
-                </ul>
+                <div className="col-4">
+                  <ul>
+                    <li>
+                      Current temperature :
+                      <span id="current-temperature">
+                        {Math.round(weatherData.temperature)}
+                      </span>
+                      ℃
+                    </li>
+                    <li>
+                      Humidity:{" "}
+                      <span id="humidity">{weatherData.humidity}</span>%
+                    </li>
+                    <li>
+                      Wind: <span id="wind">{weatherData.wind}</span>km/h
+                    </li>
+                  </ul>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-        <hr />
-        <div className="weatherForecast" id="forecast">
-          <div className="row">
-            <ColWeatherForecast />
-            <ColWeatherForecast />
-            <ColWeatherForecast />
-            <ColWeatherForecast />
-            <ColWeatherForecast />
+          <hr />
+          <div className="weatherForecast" id="forecast">
+            <div className="row">
+              <ColWeatherForecast />
+              <ColWeatherForecast />
+              <ColWeatherForecast />
+              <ColWeatherForecast />
+              <ColWeatherForecast />
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    const apiKey = "1d69840c0c590c7b98248b4102610f33";
+    let city = "Madrid";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+  }
 }
